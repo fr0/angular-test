1a26ad541a849e9f37bb98b541a05878d3a7ee82
Chris Frolik <cfrolik@beckman.com>
2019-02-05 13:28:31 -0500

    add foo.component.ts

---
+++ src/app/foo.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-foo',
+  template: `
+    this is a component
+  `,
+  styles: [`
+
+  `]
+})
+export class FooComponent {
+
+}
