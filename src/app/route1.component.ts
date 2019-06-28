dd5e56db953e9328487c7ba6e58a682be77357fa
Chris Frolik <cfrolik@beckman.com>
2019-06-28 17:36:20 -0400

    remove extraneous styles

--- src/app/route1.component.ts
+++ src/app/route1.component.ts
@@ -6,28 +6,7 @@ import {Component} from '@angular/core';
     <label [style.opacity]="opacity">
       This has an opacity binding, and should not be visible
     </label>
-  `,
-  styles: [`
-    :host {
-      display: flex;
-      flex-direction: column;
-    }
-    label {
-      margin-bottom: 20px;
-    }
-    div[header] {
-      font-weight: bold;
-    }
-    div[body] {
-      margin-left: 20px;
-    }
-    .code {
-      font-family: monospace;
-    }
-    app-expandable, app-expandable-if {
-      margin-bottom: 6px;
-    }
-  `]
+  `
 })
 export class Route1Component {
   opacity = 0;
