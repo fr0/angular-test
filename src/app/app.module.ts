2a8bdba9e8027983180979f94d91a94f37de28d9
Chris Frolik <cfrolik@beckman.com>
2019-07-30 17:22:09 -0400

    update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import {FormsModule} from '@angular/forms';
 import {Route1Component} from './route1.component';
 import {Router, RouterModule, Routes} from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {UIComponentsModule} from 'lib-test';
 
 const routes: Routes = [
   {path: 'route1', component: Route1Component},
@@ -22,7 +21,6 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    UIComponentsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/route1.component.ts
+++ src/app/route1.component.ts
@@ -3,12 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-route1',
   template: `
-    toolbar buttons below should be visible.
-    <lib-toolbar>
-      <lib-toolbar-button text="Button One" icon="/assets/circle-fill.svg"></lib-toolbar-button>
-      <lib-toolbar-button text="Button Two" icon="/assets/circle-stroke.svg"></lib-toolbar-button>
-    </lib-toolbar>
-
+    route1
   `
 })
 export class Route1Component {
